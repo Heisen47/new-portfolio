b673c3b7f04dca70ad22036b5330c6e15622e9d5
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2025-04-20 23:24:19 +0530

    add : vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,30 @@
 import { Inter } from 'next/font/google'
 import '@/app/globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
-const inter = Inter({ 
+const inter = Inter({
   subsets: ['latin'],
-  variable: '--font-inter',
+  variable: '--font-inter'
 })
 
 export const metadata = {
   title: 'My-Portfolio',
-  description: 'My portfolio website',
+  description: 'My portfolio website'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang='en' className={inter.variable}>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </head>
-      <body className="bg-background">
+      <body className='bg-background'>
         {children}
+        <Analytics />
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Hero from '@/components/sections/Hero'
 import About from '@/components/sections/About'
 import Projects from '@/components/sections/Projects'
 import Skills from '@/components/sections/Skills'
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function Page() {
